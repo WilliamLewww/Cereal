d02c9b1da45482f39d0d0c32a70356595c7fd70e
William Lew <WilliamMilesLew@gmail.com>
2017-08-17 01:27:49 -0700

    capture region of interest

--- cereal.py
+++ cereal.py
@@ -1,17 +1,37 @@
-import cv2, sys
+from network_min import *
+import cv2
+
+np.set_printoptions(suppress = True);
 
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 video_capture = cv2.VideoCapture(0)
+
+roi = [[] for _ in range(4)]
+
+def grab_pixels(x, y, w, h, image):
+    roi = [[] for _ in range(4)]
+    sample = image[y:y+h, x:x+h]
+
+    count = index = 0
+    for col in sample:
+        for pixel in col:
+            if (count >= (w * h) / 4):
+                index += 1
+                count = 0
+
+            roi[index].append(pixel)
+            count += 1;
 
 while True:
     ret, frame = video_capture.read()
-    if ret==True:
+    if (ret == True):
         gray_eye = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         detected_eyes = eyeCascade.detectMultiScale(gray_eye, 1.3, 5)
 
         for (x, y, w, h) in detected_eyes:
+            if cv2.waitKey(1) & 0xFF == ord('w'):
+                grab_pixels(x, y, w, h, frame)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.line(frame, (x, y), ((x + w,y + h)), (0, 0, 255), 1)
             cv2.line(frame, (x + w, y), ((x, y + h)), (0, 0, 255), 1)
@@ -21,6 +41,5 @@ while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-# When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
